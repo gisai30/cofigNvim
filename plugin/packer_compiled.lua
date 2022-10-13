56f473124562fd4626d167494d52faceffdd570e
Getsemani Ibarra <getsemaniibarra@Getsemanis-MacBook-Pro.local>
2022-10-13 00:38:37 -0500

    update gitignore

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/giie/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/giie/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/giie/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/giie/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/giie/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/getsemaniibarra/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/getsemaniibarra/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/getsemaniibarra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/getsemaniibarra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/getsemaniibarra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,122 +76,127 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["bufferline.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/bufferline.nvim",
     url = "https://github.com/akinsho/bufferline.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
+  ["dashboard-nvim"] = {
+    loaded = true,
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/dashboard-nvim",
+    url = "https://github.com/glepnir/dashboard-nvim"
   },
   ["hop.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/hop.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/hop.nvim",
     url = "https://github.com/phaazon/hop.nvim"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["lspkind.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
     url = "https://github.com/onsails/lspkind.nvim"
   },
   ["lspsaga.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/lspsaga.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/lspsaga.nvim",
     url = "https://github.com/glepnir/lspsaga.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["modes.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/modes.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/modes.nvim",
     url = "https://github.com/mvllow/modes.nvim"
   },
   ["nord-vim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nord-vim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nord-vim",
     url = "https://github.com/arcticicestudio/nord-vim"
   },
   ["nvim-autopairs"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-colorizer.lua"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
     url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-ts-autotag"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
     url = "https://github.com/windwp/nvim-ts-autotag"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["onedark.vim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/onedark.vim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/onedark.vim",
     url = "https://github.com/joshdick/onedark.vim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-commentary"] = {
     loaded = true,
-    path = "/Users/giie/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    path = "/Users/getsemaniibarra/.local/share/nvim/site/pack/packer/start/vim-commentary",
     url = "https://github.com/tpope/vim-commentary"
   }
 }
